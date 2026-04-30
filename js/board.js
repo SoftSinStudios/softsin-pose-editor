06bd50e0fb785a3e6e67e2c53fc94fa85a05f041
SoftSin Studios <davidpolensky@gmail.com>
2026-04-30 12:48:45 -0500

    Update board.js

--- js/board.js
+++ js/board.js
@@ -1269,7 +1269,6 @@ async function startBoardPresence() {
 
   const channel = supabase.channel("softsin-board-online", {
     config: {
-      private: true,
       presence: {
         key: currentUser.id
       }
